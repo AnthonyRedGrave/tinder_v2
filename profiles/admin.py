7c22eee91b1bdbbbc9fcba3e2d0f7f16fe588bc2
Oleg <obarnev@inbox.ru>
2021-11-22 15:59:35 +0300

    add models to admin, tabular inline, show images

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
-from .models import Profile, Gallery, Photo 
+from .models import Language, Profile, Gallery, Photo, ProfileInformation, Favorites
+from django.contrib.admin.widgets import AdminFileWidget
+from django.db import models
+from django.utils.safestring import mark_safe
 
 admin.site.register(Profile)
-admin.site.register(Gallery)
+
+class AdminPhotoWidget(AdminFileWidget):
+    def render(self, name, value, attrs = None, renderer=None):
+        output = []
+        if value and getattr(value, "url", None):
+            image_url = value.url
+            file_name = str(value)
+            output.append(u' <a href="%s" target="_blank"><img src="%s" alt="%s" width="150" height="150"  style="object-fit: cover;"/></a>' % (image_url, image_url, file_name))
+        output.append(super(AdminFileWidget, self).render(name, value, attrs))
+        return mark_safe(u''.join(output))
+
+
+class PhotoInline(admin.TabularInline):
+    model = Photo
+    formfield_overrides = {models.ImageField: {'widget': AdminPhotoWidget}}
+    extra = 3
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ("profile",)
+    list_display_links = ("profile",)
+    inlines = (PhotoInline,)
+
 admin.site.register(Photo)
+admin.site.register(Favorites)
+admin.site.register(ProfileInformation)
+admin.site.register(Language)
--- profiles/models.py
+++ profiles/models.py
@@ -41,3 +41,62 @@ class Gallery(models.Model):
     class Meta:
         verbose_name = "Галерея профилей"
         verbose_name_plural = "Галереи профилей"
+
+
+class Favorites(models.Model):
+    title = models.CharField("Название тега", max_length=60)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+
+
+class Language(models.Model):
+    class LanguageChoices(models.TextChoices):
+        russian = "russian", "Русский"
+        english = "english", "Английский"
+        belarussian = "belarussian", "Беларусский"
+        francian = "francian", "Французский"
+        italian = "italian", "Итальянский"
+    
+    title = models.CharField("Язык", max_length=15, choices=LanguageChoices.choices)
+    
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = "Язык"
+        verbose_name_plural = "Языки"
+
+
+class ProfileInformation(models.Model):
+
+    class GenderChoices(models.TextChoices):
+        male = "male", "Мужской"
+        female = "female", "Женский"
+
+    class EducationChoices(models.TextChoices):
+        higher = "higher", "Высшее"
+        secondary = "secondary", "Среднее"
+        school = "school", "Школьное"
+
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='profile_information')
+    gender = models.CharField("Пол", choices=GenderChoices.choices, max_length=20)
+    languages = models.ManyToManyField(Language, verbose_name="Языки")
+    education = models.CharField("Образование", choices=EducationChoices.choices, max_length=30)
+    weight = models.DecimalField("Вес", decimal_places=1, max_digits=4, default=0)
+    description = models.TextField("Описание профиля")
+    location = models.CharField("Место расположения", max_length=150)
+    favorites = models.ManyToManyField(Favorites, related_name="profile_information")
+    registration_date = models.DateTimeField("Дата регистрации профиля", auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Подробная информация профиля {self.profile.user.username}"
+
+    class Meta:
+        verbose_name = "Информация о профиле"
+        verbose_name_plural = "Информации о профилях"
+
